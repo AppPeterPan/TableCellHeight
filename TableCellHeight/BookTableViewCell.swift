e07f32622ecd9cb618062cc2763c82c717f36522
Shr Ying Pan <apppeterpan@gmail.com>
2017-12-24 02:07:19 +0800

    update

---
+++ TableCellHeight/BookTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BookTableViewCell.swift
+//  TableCellHeight
+//
+//  Created by SHIH-YING PAN on 24/12/2017.
+//  Copyright © 2017 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
